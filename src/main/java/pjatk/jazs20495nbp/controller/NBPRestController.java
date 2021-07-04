3713e1f3c112cb92c7b963e77b933f5dd6e8c162
Wadas <mawd@gft.com>
2021-07-04 18:43:49 +0200

    doc

--- src/main/java/pjatk/jazs20495nbp/controller/NBPRestController.java
+++ src/main/java/pjatk/jazs20495nbp/controller/NBPRestController.java
@@ -1,6 +1,11 @@
 package pjatk.jazs20495nbp.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pjatk.jazs20495nbp.model.Gold;
 import pjatk.jazs20495nbp.service.NbpService;
 
 
@@ -14,7 +19,16 @@ public class NBPRestController {
         this.nbpService = nbpService;
     }
 
-//    @GetMapping(value = "/gold", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<> calculateCurrency(@PathVariable String currency, @RequestParam(defaultValue = "1")
-//    }
+//    @ApiOperation(value =  "Average exchange rate",
+//            response = Gold.class,
+//            notes = "Average exchange rate between dates")
+//
+//    @ApiResponses(value = {
+//            @ApiResponse(code = 200, message = "Perfect!"),
+//            @ApiResponse(code = 401, message = "Access denied"),
+//            @ApiResponse(code = 404, message = "Not Found"),
+//            @ApiResponse(code = 403, message = "Forbidden")
+//    })
+
+
 }
