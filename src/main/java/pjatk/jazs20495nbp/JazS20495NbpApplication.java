fe61a060b5473961c45dec712308c6d6aedab95f
Wadas <mawd@gft.com>
2021-07-04 16:40:16 +0200

    first commit

---
+++ src/main/java/pjatk/jazs20495nbp/JazS20495NbpApplication.java
@@ -0,0 +1,13 @@
+package pjatk.jazs20495nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20495NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20495NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjatk/jazs20495nbp/JazS20495NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.jazs20495nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20495NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
